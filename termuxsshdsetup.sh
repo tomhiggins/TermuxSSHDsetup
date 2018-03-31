d6da35d378d9e98ab947fc2552d84be090d000bb
T4P4N <32695903+T4P4N@users.noreply.github.com>
2018-03-31 12:47:53 +0530

    Update termuxsshdsetup.sh

--- termuxsshdsetup.sh
+++ termuxsshdsetup.sh
@@ -1,14 +1,30 @@
-# A script to set up sshd into a termux install.
-#
-apt update
-apt upgrade
-termux-setup-storage
+#!/data/data/com.termux/files/usr/bin/bash
+echo "Installing openssh"
 apt install openssh
-touch ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-chmod 700 ~/.ssh
+#genrating keys
 ssh-keygen
+echo "Your Key is now Genrated"
+sleep 1
+#changing permission of keys
+echo "Changing permission of keys"
+sleep 1
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
+echo "Copying Key to sdcard"
+cp ~/.ssh/id_rsa /sdcard
+sleep 2
+echo "Your key Is copied to sdcard"
+echo "Now send the key to ssh client device"
+echo "And move the key to home dir of termux"
+#Starting SSH Server
+sleep 2
+echo "Starting SSH server"
 sshd
-termux-open-url https://github.com/tomhiggins/TermuxSSHDsetup
+sleep 1
+echo "Done!"
+Results = "$(whoami)"
+echo "ssh ${Results}@IP -p 8022 -i $HOME/id_rsa"
+echo "Now use the command above to ssh"
+echo "into Termux."
+echo "<<<<Script By T4P4N>>>>"
+echo "Special Thanks to Neo-oli"
